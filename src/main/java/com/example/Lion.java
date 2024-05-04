b7318181e5a72a942bc5256c226bdbf795023e8a
Harry Bobrov <harry.beaver@yandex.ru>
2024-05-04 23:09:31 +0200

    add tests

--- src/main/java/com/example/Lion.java
+++ src/main/java/com/example/Lion.java
@@ -3,8 +3,14 @@ package com.example;
 import java.util.List;
 
 public class Lion {
-
+    Feline feline;
+    String sex;
     boolean hasMane;
+
+    public Lion(String sex, Feline feline) {
+        this.sex = sex;
+        this.feline = feline;
+    }
 
     public Lion(String sex) throws Exception {
         if ("Самец".equals(sex)) {
@@ -15,8 +21,6 @@ public class Lion {
             throw new Exception("Используйте допустимые значения пола животного - самей или самка");
         }
     }
-
-    Feline feline = new Feline();
 
     public int getKittens() {
         return feline.getKittens();
---
+++ src/test/java/CatTest.java
@@ -0,0 +1,34 @@
+import com.example.Cat;
+import com.example.Feline;
+import java.util.List;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CatTest {
+  @Mock
+  Feline mockFeline;
+
+  @Test
+  public void testGetSound() {
+    Cat kitty = new Cat(mockFeline);
+    String actualSound = kitty.getSound();
+    String expectedSound = "Мяу";
+    Assert.assertEquals(expectedSound, actualSound);
+  }
+
+  @Test
+  public void testGetFood() throws Exception {
+    Cat kitty = new Cat(mockFeline);
+    List<String> expectedList = List.of("Животные", "Птицы", "Рыба");
+    Mockito.when(mockFeline.eatMeat()).thenReturn(expectedList);
+    Assert.assertEquals(expectedList, kitty.getFood());
+    System.out.println("Actual Result: " + mockFeline.eatMeat());
+    System.out.println("Expected Result: " + expectedList);
+  }
+
+}
